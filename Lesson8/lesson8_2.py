d6826bc62f11367e6ffff6c61c2f9e7c89845978
peterlu22 <peterlu22@gmail.com>
2024-11-10 11:47:01 +0800

    modify

--- Lesson8/lesson8_2.py
+++ Lesson8/lesson8_2.py
@@ -4,27 +4,54 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-conn = psycopg2.connect(host=os.environ['POSTGRE_HOST'],database=os.environ['POSTGRE_DATABASE'],user=os.environ['POSTGRE_USER'],password=os.environ['POSTGRE_PASSWORD'])
-with conn:
-    with conn.cursor() as cursor:
-        sql = '''
-        SELECT * FROM 市場
-        '''
-        cursor.execute(sql)
-        all_country = cursor.fetchall()
-conn.close()
+@st.cache_resource
+def getData():
+    conn = psycopg2.connect(host=os.environ['POSTGRE_HOST'],
+                            database=os.environ['POSTGRE_DATABASE'],
+                            user=os.environ['POSTGRE_USER'],
+                            password=os.environ['POSTGRE_PASSWORD'])
+    with conn:
+        with conn.cursor() as cursor:
+            sql = '''
+            SELECT country,市場.name,date,adj_close,volume
+            FROM 股市 JOIN 市場 ON 股市.name = 市場.name
+            WHERE country = '台灣';
+            '''
+            cursor.execute(sql)
+            all_data = cursor.fetchall()
+    conn.close()
+    return all_data
+
+
+@st.cache_resource
+def get_country():
+    conn = psycopg2.connect(host=os.environ['POSTGRE_HOST'],
+                            database=os.environ['POSTGRE_DATABASE'],
+                            user=os.environ['POSTGRE_USER'],
+                            password=os.environ['POSTGRE_PASSWORD'])
+    with conn:
+        with conn.cursor() as cursor:
+            sql = '''
+            SELECT * FROM 市場
+            '''
+            cursor.execute(sql)
+            all_country = cursor.fetchall()
+    conn.close()
+    input_dict = dict(all_country)
+    return input_dict.values()
 
 def user_select():
     print('使用者選擇了')
     print(st.session_state.stocks)
 
+print(getData())
 
 st.title('世界大盤分析')
 with st.sidebar:
     default_country = '台灣'
     st.title('請選擇股票市場:')
-    input_dict = dict(all_country)
-    st.multiselect("請選擇",input_dict.values(),
+    
+    st.multiselect("請選擇",get_country(),
                     default=default_country,
                     placeholder="請選擇市場",
                     label_visibility='hidden',
